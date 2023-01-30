7b7cb12651bc4862afa913bbc25eb16decb23b7a
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-01-30 14:53:06 -0600

    Added button and linked function to show nonwinter bike

--- assignment/client.js
+++ assignment/client.js
@@ -1,8 +1,20 @@
-/*function showNonWinter(){
+let nonWinterIsDisplayed = false;
 
+function showNonWinter(){
+    if(nonWinterIsDisplayed === false){
+        let nonWinter = document.querySelector('#nonWinterHtml');
+        nonWinter.innerHTML += `
+        <img id="bike" src="bike.jpg" alt="A sweet all-black Surly Straggler">
+        <h3>Surly Straggler if it's not winter.</h3> 
+        `;
+        nonWinterIsDisplayed = true;
+    } else {
+        let nonWinter = document.querySelector('#nonWinterHtml');
+        nonWinter.innerHTML = ''
+        nonWinterIsDisplayed = false;
+    }
 }
 
-function showWinter(){
+/*function showWinter(){
 
-}
-*/
+}*/
