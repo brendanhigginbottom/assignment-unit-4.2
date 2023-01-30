c204e259c2964726fd1ef1790b1004b13fdcadd9
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-01-30 15:13:31 -0600

    Added logic to buttontext to change with whether function has run to append to DOM

--- assignment/client.js
+++ assignment/client.js
@@ -1,5 +1,7 @@
 let nonWinterIsDisplayed = false;
 let winterIsDisplayed = false;
+let nonWinterButton;
+let winterButton;
 
 function showNonWinter(){
     if(nonWinterIsDisplayed === false){
@@ -8,10 +10,14 @@ function showNonWinter(){
         <img id="bike" src="bike.jpg" alt="A sweet all-black Surly Straggler">
         <h3>Surly Straggler if it's not winter.</h3> 
         `;
+        nonWinterButton = document.querySelector('#notWinter');
+        nonWinterButton.innerHTML = 'Click to hide non-winter bike';
         nonWinterIsDisplayed = true;
     } else {
         let nonWinter = document.querySelector('#nonWinterHtml');
         nonWinter.innerHTML = ''
+        nonWinterButton = document.querySelector('#notWinter');
+        nonWinterButton.innerHTML = 'Click to see non-winter bike';
         nonWinterIsDisplayed = false;
     }
 }
@@ -24,10 +30,14 @@ function showWinter(){
         <h3>Trek Antelope (with Antelope scratched out to just read "ope") 
         in the winter.</h3> 
         `;
+        winterButton = document.querySelector('#winter');
+        winterButton.innerHTML = 'Click to hide winter bike';
         winterIsDisplayed = true;
     } else {
         let winter = document.querySelector('#winterHtml');
         winter.innerHTML = ''
+        winterButton = document.querySelector('#winter');
+        winterButton.innerHTML = 'Click to see winter bike';
         winterIsDisplayed = false;
     }
 }
